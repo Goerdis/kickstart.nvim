5a38d7a4122812babab9b592c49421331b0a0511
Thomas <thomas.jadal@sitevision.se>
2023-09-21 11:21:13 +0200

    custom changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.cmd [[source ~/.config/nvim/vimscript/custom_bindings.vim]]
+
+--vim.api.nvim_create_autocmd("VimEnter", {
+--  command = "set nornu nonu | Neotree toggle",
+--})
+--vim.api.nvim_create_autocmd("BufEnter", {
+--  command = "set rnu nu",
+--})
+
+vim.keymap.set('n', '<C-l>', '<Cmd>Neotree toggle<CR>')
+
 return {}
---
+++ vimscript/custom_bindings.vim
@@ -0,0 +1,17 @@
+set relativenumber
+
+set autoindent
+noremap <C-Up> 5k
+noremap <C-Down> 5j
+noremap <C-Left> 5h
+noremap <C-Right> 5l
+set foldlevelstart=20
+set scrolloff=8
+set sidescrolloff=8
+set confirm
+set cursorline
+
+inoremap <C-v> <ESC>"+pa
+vnoremap <C-c> "+y
+vnoremap <C-d> "+d
+
